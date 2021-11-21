79dea66a2fed655b9efa5cf64b77433fe3d346ff
dada92 <982552295@qq.com>
2021-11-21 22:52:03 +0800

    添加路由

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"gin_vue_study/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.POST("/api/auth/register", controller.Register)
+	r = CollectRoute(r)
 	panic(r.Run(":8080"))
 	return
 }
